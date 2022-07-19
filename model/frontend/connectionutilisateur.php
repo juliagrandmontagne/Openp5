6dc19c6aae476265e66d39a15cf37700b06021ce
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-07-19 14:18:53 +0200

    Add files via upload

--- model/frontend/connectionutilisateur.php
+++ model/frontend/connectionutilisateur.php
@@ -4,31 +4,24 @@ class modelconnectionutilisateur
     public function seconnecter()  
     {
       require('connexion.php');
-    
     }
-
-   
-
     public function connexionutilisateur()  
     {
       require('connexion.php');
-   // header('Location:index.php?action=utilisateur');
-   ///$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE PseudoUtilisateur = ?,	MpUtilisateur = ?');
-   //$namesaisie = htmlspecialchars(['login']);
-   //$mpsaisie = htmlspecialchars(['mp']);  
-   //$reponse->execute( array($namesaisie ,$mpsaisie ));
-   //$userexist = $reponse->rowcount();
-   //if($userexist= 1, ,) 
-   //{
-    // session_start (); 
-    // header('Location:index.php?action=utilisateur')
-  // }
-  // else{ echo"vous n'existez pas ";} 
-  //$data = $reponse->fetchAll();
-  //return $data;
-   //$rereponseq->cloreCursor();
+   $reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE PseudoUtilisateur = ?,	MpUtilisateur = ?');
+   $namesaisie = htmlspecialchars(['login']);
+   $mpsaisie = htmlspecialchars(['mp']);  
+   $reponse->execute( array($namesaisie ,$mpsaisie ));
+   $userexist = $reponse->rowcount();
+   if($userexist > 0) 
+   {
+     header('Location:index.php?action=utilisateur');
+   }
+   else{ echo"vous n'existez pas ";} 
+  $data = $reponse->fetchAll();
+  return $data;
+   $rereponseq->cloreCursor();
     }
-
     public function connexionadministrateur()  
     {
       require('connexion.php');
