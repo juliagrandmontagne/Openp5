6a81282bffb3991e3ac938049a88eed6ed9ec356
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-09-03 18:26:42 +0200

    Add files via upload

--- View/Frontend/GestionUtilisateur.php
+++ View/Frontend/GestionUtilisateur.php
@@ -2,31 +2,33 @@
 <?php ob_start() ; ?>
   <section id="main">
   <form action="index.php?action=PageCreationEdition" method="post">
-  <div class="ajout">ajouter une recette
-<button  type="submit" class="boutton"><i class="fa-solid fa-plus" aria-hidden="true"class="icone"></i></button>
+  <div class="ajout">
+<button  type="submit" class="boutton"><p class="ajout">ajouter une recette</p></button>
 </form> 
 </div>
-    <div class="carrégestionrecette">
+    <div class="carregestionrecette">
       <div class="recherche">
         <label for="site-search"></label>
         <input type="search" id="site-search" name="q">
-        <button>chercher</button>
-        
+        <button id="chercher">chercher</button>
       </div>
         <div class="liste">
         <?php  foreach ($DataUtilisateur as $donnees) : ?>
-          <label class="titrerecetteutilisateur"><?php echo $donnees['nomrecette']?></label>
+          <div class="elementrecette">
+          <h2 class="titrerecetteutilisateur"><?php echo $donnees['nomrecette']?></h2>
           <form action="index.php?action=SupressionRecette" method="post" class="listeutilisateur">
-  <div class="iconemodifier">
-    <input type="text">
-                <button  type="submit" class="icone"><i class="fa fa-trash" aria-hidden="true" class="icone" class="listeutilisateur"></i></button>
-            </form>          
-            <form action="index.php?action=EditionRecette" method="post" class="listeutilisateur">
+           <div class="iconeactionrecette">
+            <input class="none" value="<?php echo $donnees['ID']?>">
+              <button  type="submit" class="icone"><i class="fa fa-trash" aria-hidden="true" class="listeutilisateur"></i></button>
+          </form>          
+          <form action="index.php?action=EditionRecette" method="post" class="listeutilisateur">
+             <input class="none" value="<?php echo $donnees['ID']?>"> 
               <button  type="submit" class="icone"> <i class='fas fa-edit'class="icone" ></i></button>
-</div>
-            </form>
-        <?php endforeach; ?>
-      </div>
+          </form>
+           </div>
+           </div>
+        <?php endforeach;?>
+       </div>
     </div>
   </section>   
 <?php $content = ob_get_clean() ; ?>
--- projet5.sql
+++ projet5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mar. 30 août 2022 à 13:10
+-- Généré le : lun. 01 août 2022 à 14:45
 -- Version du serveur :  5.7.32
 -- Version de PHP : 7.4.12
 
@@ -45,7 +45,7 @@ INSERT INTO `recette` (`ID`, `Ingredients`, `nomrecette`, `Recette`, `IDUtilisat
 (3, '4 cl de rhum blanc\r\n6 feuilles de menthe\r\n1 eau gazeuse\r\n2 cl de sirop de sucre de canne\r\n½ citron vert\r\n10 glaçons', 'Mojito', 'Coupez le citron en deux puis chaque demi citron en 6 morceaux.\r\nAjoutez le sirop de sucre de canne. Le fait d\'utiliser un sucre liquide permet de ne pas sentir les cristaux du sucre à la dégustation du cocktail.Ajoutez la glace.Ajoutez le rhum.Complétez avec l\'eau gazeuse.Mélangez le cocktail afin que les saveur se mêlent.', 1, 0, 1),
 (4, '4 cl de rhum blanc\r\n6 feuilles de menthe\r\n1 eau gazeuse\r\n2 cl de sirop de sucre de canne\r\n½ citron vert\r\n10 glaçons', 'Mojito', 'Coupez le citron en deux puis chaque demi citron en 6 morceaux.\r\nAjoutez le sirop de sucre de canne. Le fait d\'utiliser un sucre liquide permet de ne pas sentir les cristaux du sucre à la dégustation du cocktail.Ajoutez la glace.Ajoutez le rhum.Complétez avec l\'eau gazeuse.Mélangez le cocktail afin que les saveur se mêlent.', 1, 0, 1),
 (5, '4 cl de rhum blanc\r\n6 feuilles de menthe\r\n1 eau gazeuse\r\n2 cl de sirop de sucre de canne\r\n½ citron vert\r\n10 glaçons', 'Mojito', 'Coupez le citron en deux puis chaque demi citron en 6 morceaux.\r\nAjoutez le sirop de sucre de canne. Le fait d\'utiliser un sucre liquide permet de ne pas sentir les cristaux du sucre à la dégustation du cocktail.Ajoutez la glace.Ajoutez le rhum.Complétez avec l\'eau gazeuse.Mélangez le cocktail afin que les saveur se mêlent.', 1, 0, 1),
-(6, '4 cl de rhum blanc\r\n2 cl de rhum ambré\r\n12 cl de jus d\'ananas\r\n4 cl de lait de coco ', 'Piña Colada ', '\r\n    Réalisez la recette \"Piña Colada\" au mixer.\r\n    Dans un blender (mixer), versez les ingrédients avec 5 ou 6 glaçons et mixez le tout. C\'est prêt ! Versez dans le verre et dégustez. Peut aussi se réaliser au shaker si c\'est juste pour une personne.\r\n    Servir dans un verre de type \"verre à vin\".\r\n    Décorer avec un morceau d\'ananas et une cerise confite.\r\n', 4, 0, 1);
+(6, '4 cl de rhum blanc\r\n6 feuilles de menthe\r\n1 eau gazeuse\r\n2 cl de sirop de sucre de canne\r\n½ citron vert\r\n10 glaçons', 'Mojito', 'Coupez le citron en deux puis chaque demi citron en 6 morceaux.\r\nAjoutez le sirop de sucre de canne. Le fait d\'utiliser un sucre liquide permet de ne pas sentir les cristaux du sucre à la dégustation du cocktail.Ajoutez la glace.Ajoutez le rhum.Complétez avec l\'eau gazeuse.Mélangez le cocktail afin que les saveur se mêlent.', 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ INSERT INTO `recette` (`ID`, `Ingredients`, `nomrecette`, `Recette`, `IDUtilisat
 CREATE TABLE `utilisateur` (
   `PseudoUtilisateur` text NOT NULL,
   `MailUtilisateur` text NOT NULL,
-  `IDUtilisateur` int(255) NOT NULL,
+  `IDRecette` int(255) NOT NULL,
   `MpUtilisateur` text NOT NULL,
   `Administrateur` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -65,11 +65,9 @@ CREATE TABLE `utilisateur` (
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`PseudoUtilisateur`, `MailUtilisateur`, `IDUtilisateur`, `MpUtilisateur`, `Administrateur`) VALUES
+INSERT INTO `utilisateur` (`PseudoUtilisateur`, `MailUtilisateur`, `IDRecette`, `MpUtilisateur`, `Administrateur`) VALUES
 ('utilisateur', 'mail@mail.com', 1, '4afb35ee9fc9720b9b2cf59529f4332478c8650f', 0),
-('utilisateurs', 'mail@mail.com', 4, '4afb35ee9fc9720b9b2cf59529f4332478c8650f', 0),
-('utilisateure', 'mail@mail.com', 5, '4afb35ee9fc9720b9b2cf59529f4332478c8650f', 1),
-('julia', 'juju.g-r@hotmail.fr', 6, 'ba16cc8551a4f90f4886bc1b3b2f9f93526386ae', 0);
+('utilisateure', 'mail@mail.com', 5, '4afb35ee9fc9720b9b2cf59529f4332478c8650f', 1);
 
 --
 -- Index pour les tables déchargées
@@ -86,7 +84,7 @@ ALTER TABLE `recette`
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`IDUtilisateur`);
+  ADD PRIMARY KEY (`IDRecette`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -102,7 +100,7 @@ ALTER TABLE `recette`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `IDUtilisateur` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `IDRecette` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Contraintes pour les tables déchargées
@@ -112,7 +110,7 @@ ALTER TABLE `utilisateur`
 -- Contraintes pour la table `recette`
 --
 ALTER TABLE `recette`
-  ADD CONSTRAINT `lien utilisateur-recette` FOREIGN KEY (`IDUtilisateur`) REFERENCES `utilisateur` (`IDUtilisateur`);
+  ADD CONSTRAINT `lien utilisateur-recette` FOREIGN KEY (`IDUtilisateur`) REFERENCES `utilisateur` (`IDRecette`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
