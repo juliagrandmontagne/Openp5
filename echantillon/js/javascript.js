36307dc6ae347d3e59bbd03e6d965cf522ae20e0
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-11-24 14:57:09 +0100

    Delete echantillon directory

--- echantillon/js/javascript.js
+++
@@ -1,8 +0,0 @@
-const dataCouleur = ["Blanc","bleu"];
-const dataCode = ["#ffff","#bde0ff"];
-dataCouleur.forEach(function(couleur){
-	document.getElementById('afficherCouleur').innerHTML += couleur;
-});
-dataCode.forEach(function(Code){
-	document.getElementById('afficherCode').innerHTML += Code;
-});
