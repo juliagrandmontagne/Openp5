64d68fe35aace70269108cc6387d05fe302c2c9d
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-12-15 13:49:00 +0100

    Add files via upload

--- Model/Frontend/Recette.php
+++ Model/Frontend/Recette.php
@@ -4,11 +4,51 @@ class Recette
     public function RecupereRecetteDebutant()  
     {
         require('Connexion.php');
-        $reponse = $bdd->prepare('SELECT * FROM recette WHERE difficulte =1');
+        
+        if(isset($_GET['oh']))
+        {
+            if($_GET['oh']==="Rhum"){
+                echo"Selection Rhum";
+                //$reponse = $bdd->prepare('SELECT * FROM recette WHERE Ingredients LIKE '%Rhum%' AND difficulte=1 ');
+                //$reponse->execute();
+                //$data = $reponse->fetchAll();
+                //return $data;
+                //$rereponseq->cloreCursor();
+            }
+            if($_GET['oh']=="Vodka"){
+                echo"Selection Vodka";
+                //$reponse = $bdd->prepare('SELECT * FROM recette WHERE Ingredients LIKE '%Rhum%' AND difficulte=1 ');
+                //$reponse->execute();
+                //$data = $reponse->fetchAll();
+                //return $data;
+                //$rereponseq->cloreCursor();
+            }
+            if($_GET['oh']=="Whisky"){
+                echo"selection Whisky";
+                //$reponse = $bdd->prepare('SELECT * FROM recette WHERE Ingredients LIKE '%Rhum%' AND difficulte=1 ');
+     
+            }
+            if($_GET['oh']=="Scotch"){
+                echo"selection scotch";
+                //$reponse = $bdd->prepare('SELECT * FROM recette WHERE Ingredients LIKE '%Rhum%' AND difficulte=1 ');
+           
+            }
+            if($_GET['oh']=="Tequila"){
+                echo"selection tequila";
+            }
+            if($_GET['oh']=="Champagne"){
+                echo"selection champagne";
+            }
+            else
+            {  echo"bien essayer ;)!";}
+           
+        }else
+        {
+        $reponse = $bdd->prepare('SELECT * FROM recette WHERE  difficulte =1 ');
         $reponse->execute();
         $data = $reponse->fetchAll();
         return $data;
-        $rereponseq->cloreCursor();
+        $rereponseq->cloreCursor();}
     }
     public function RecupereRecetteIntermediaire()  
     {
--- View/Frontend/RecetteDifficulter1.php
+++ View/Frontend/RecetteDifficulter1.php
@@ -4,13 +4,12 @@
     <aside class="MenuFiltreAlcool">
       <nav class="nav" >
         <ul>
-          <li><a class="oh" href="#">Rhum</a></li></br>
-          <li><a class="oh" href="#">Vodka</a></li>
-          <li><a class="oh" href="#">Whisky </a></li>
-          <li><a class="oh" href="#">Scotch</a></li>
-          <li><a class="oh" href="#">Tequila</a></li>
-          <li><a class="oh" href="#">Champagne</a></li>
-          <li><a class="oh" href="#">Moktail</a></li>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Rhum">Rhum</a></li></br>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Vodka">Vodka</a></li>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Whisky">Whisky </a></li>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Scotch">Scotch</a></li>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Tequila">Tequila</a></li>
+          <li><a class="oh" href="index.php?action=PageRecetteDebutant&oh=Champagne">Champagne</a></li>
         </ul>
       </nav>
     </aside>
