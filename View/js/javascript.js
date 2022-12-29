bb64a962a4b51b0889ae6183fe1d9a616834847f
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-12-29 14:31:03 +0100

    Add files via upload

--- View/js/javascript.js
+++ View/js/javascript.js
@@ -1,15 +1,17 @@
 // Constante pour faire appel l'ID Select présent en HTML
 const selectCouleur = document.getElementById('selectCouleur');
 // Création de la fonction
-function couleur()
+class couleur()
 	{
+        constructor
         // Définir la constante pour créer la balise Option
-        dataCouleur.forEach(function(couleur, index){
-            const choixCouleur = document.createElement('option');
-            choixCouleur.innerHTML = couleur; // Insérer le texte
-            choixCouleur.value = dataCode[index] // Insérer la valeur
+        this.dataCouleur.forEach(function couleurselection(this.couleur, this.index){
+            this.choixCouleur = document.createElement('option');
+            this.choixCouleur.innerHTML = this.couleur; // Insérer le texte
+            this.choixCouleur.value = dataCode[this.index] // Insérer la valeur
             selectCouleur.appendChild(choixCouleur) // Définir l'option comme étant l'enfant de Select 
         });
+        const lancement=new couleurselection()
     }
     function background() {
         document.getElementById('Recette1').style.background = "<?php  foreach ($DataPagerecette as $donnees) : ?><?php echo $donnees['couleur']?><?php endforeach; ?>";
